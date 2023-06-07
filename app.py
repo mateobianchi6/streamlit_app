3e4f20d10082216c81acdbb13e106549d7e676ca
mateobianchi6 <mateobianchi@icloud.com>
2023-06-06 19:32:45 -0700

    editing

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 st.title("Mateo's App")
 st.subheader("Upload a CSV to analyze")
@@ -9,11 +11,10 @@ upload_df = st.file_uploader("Choose File")
 
 if upload_df is not None:
     df = pd.read_csv(upload_df)
-    st.write(df)
 
 st.subheader("Then choose a column and visit the tabs to analyze it!")
 
-st.selectbox("Choose a column", df.columns)
+col = st.selectbox("Choose a column", df.columns)
 
 tab1, tab2, tab3,  = st.tabs(['At First Glance', 'Numerical Analysis', 'Categorical Analysis'])
 
@@ -23,13 +24,20 @@ with tab1:
     st.header("At First Glance")
     nrow = df.shape[0]
     ncol = df.shape[1]
-    n_cat = 0
-    n_num = 0
-    #for i in df.info[0]:
-     #   if type(i) in ('int', 'float'):
-     #       n_num = n_num + 1
-     #   else:
-     #       n_cat = n_cat + 1
-    st.text(f"This data set has {nrow} rows and {ncol} columns, very interesting! There are {n_num} numerical variables and {n_cat} categorical variables.")
-    st.text(df.info)
+    n_cat = sum(df.dtypes != 'object')
+    n_num = sum(df.dtypes == 'object')
+    st.text(f"This data set has {nrow} rows and {ncol} columns, very interesting!") 
+    st.text(f"There are {n_num} numerical variables and {n_cat} categorical variables.")
+    st.text(f"You've chosen to take a deeper look at: {col}")
 
+with tab2:
+    st.header('Numerical Analysis')
+    if df[col].dtype != 'object':
+        st.subheader("Count and Five Number Summary")
+        st.text(df[col].describe())
+
+        st.subheader('Distribtution Plot')
+        dist = plt.hist(df[col])
+        st.write(dist)
+    else:
+        st.text('Not the right type of variable')
